60327f570d367b00420ce2d1cfa2946b7199c861
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-09-05 18:20:18 -0300

    Criados testes de lançamentos de exceção

--- test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -8,6 +8,8 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
 
 import java.util.ArrayList;
@@ -124,5 +126,50 @@ public class EncerradorDeLeilaoTest {
 		inOrder.verify(daoFalso, times(1)).atualiza(leilao1);
 		inOrder.verify(carteiroFalso, times(1)).envia(leilao1);
 		
+	}
+	
+	@Test
+	public void deveContinuarExecucaoMesmoQuandoDaoFalha() {
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(1999, 01, 01);
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("RTX 3090").naData(antiga).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("RTX 3080").naData(antiga).constroi();
+		
+		RepositorioDeLeiloes daoFalso = mock(RepositorioDeLeiloes.class);
+		EnviadorDeEmail carteiroFalso = mock(EnviadorDeEmail.class);
+		
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+		
+		doThrow(new RuntimeException()).when(daoFalso).atualiza(leilao1);
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso, carteiroFalso);
+		encerrador.encerra();
+		
+		verify(daoFalso).atualiza(leilao2);
+		verify(carteiroFalso).envia(leilao2);	
+		
+		verify(carteiroFalso, times(0)).envia(leilao1);
+	}
+	
+	@Test
+	public void deveDesistirSeDaoFalhaPraSempre() {
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(1999, 01, 01);
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("RTX 3090").naData(antiga).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("RTX 3080").naData(antiga).constroi();
+		
+		RepositorioDeLeiloes daoFalso = mock(RepositorioDeLeiloes.class);
+		EnviadorDeEmail carteiroFalso = mock(EnviadorDeEmail.class);
+		
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+		
+		doThrow(new RuntimeException()).when(daoFalso).atualiza(any(Leilao.class));
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso, carteiroFalso);
+		encerrador.encerra();	
+		
+		verify(carteiroFalso, never()).envia(any(Leilao.class));
 	}
 }
