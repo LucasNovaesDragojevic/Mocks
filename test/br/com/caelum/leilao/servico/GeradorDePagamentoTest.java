96587f73e6f99f88ab1d6472124dc55e61f32ae5
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-09-05 19:53:57 -0300

    Desacoplados métodos estáticos da Calendar para testar os mocks

--- test/br/com/caelum/leilao/servico/GeradorDePagamentoTest.java
+++ test/br/com/caelum/leilao/servico/GeradorDePagamentoTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentCaptor.forClass;
 
 import java.util.Arrays;
+import java.util.Calendar;
 
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -17,6 +18,7 @@ import br.com.caelum.leilao.dominio.Pagamento;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
 import br.com.caelum.leilao.infra.dao.RepositorioDePagamentos;
+import br.com.caelum.leilao.infra.relogio.Relogio;
 
 public class GeradorDePagamentoTest {
 
@@ -41,6 +43,35 @@ public class GeradorDePagamentoTest {
 		Pagamento pagamentoGerado = argumento.getValue();
 		
 		assertEquals(2500.00, pagamentoGerado.getValor(), 0.00001);
+	}
+	
+	@Test
+	public void deveEmpurrarParaProximoDiaUtil() {
+		RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
+		RepositorioDePagamentos pagamentos = mock(RepositorioDePagamentos.class);
+		Relogio relogio = mock(Relogio.class);
 		
+		Leilao leilao = new CriadorDeLeilao().para("RTX 3080")
+												.lance(new Usuario("João"), 2000.00)
+												.lance(new Usuario("Camila"), 2500.00)
+												.constroi();
+		
+		when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
+		
+		Calendar sabado = Calendar.getInstance();
+		sabado.set(2012, Calendar.APRIL, 7);
+		
+		when(relogio.hoje()).thenReturn(sabado);
+				
+		GeradorDePagamento gerador = new GeradorDePagamento(leiloes, pagamentos, new Avaliador(), relogio);
+		gerador.gera();
+		
+		ArgumentCaptor<Pagamento> argumento = forClass(Pagamento.class);
+		verify(pagamentos).salva(argumento.capture());
+		
+		Pagamento pagamentoGerado = argumento.getValue();
+		
+		assertEquals(Calendar.MONDAY, pagamentoGerado.getData().get(Calendar.DAY_OF_WEEK));
+		assertEquals(9, pagamentoGerado.getData().get(Calendar.DAY_OF_MONTH));
 	}
 }
